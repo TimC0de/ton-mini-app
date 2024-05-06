7bd49bec740f584d891361e4480bbe7b54bb47e3
Tim Pavlov <timpav1100@gmail.com>
2024-05-06 14:08:28 +0300

    Update dependencies

--- configure.js
+++ configure.js
@@ -1,15 +1,4 @@
 import axios from "axios";
-import { createInterface } from "readline";
-import fs from "fs";
-import { promisify } from "util";
-
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const question = (question) =>
-  new Promise((resolve) => rl.question(question, resolve));
 
 function exitError(error) {
   console.error(`Error! ${error}`);
@@ -26,42 +15,11 @@ const banner = `
 `;
 
 console.log(banner);
-
-let githubUsername, githubRepo, botUsername;
-
 (async () => {
-  try {
-    const file = fs.readFileSync(".git/config").toString();
-    const url = file.match(/url = (.*)/)[1];
-    console.log(url);
-    const params = url.match(/github.com[/:]([^/]*)\/(.*)\.git/);
-    githubUsername = params[1];
-    githubRepo = params[2];
-  } catch (e) {}
 
-  const accessToken = await question("Enter your bot access token: ");
-  if (!accessToken?.length > 0) exitError("Token is required");
-
-  const githubUsernameQ = await question(
-    `Enter your github username${
-      githubUsername ? ` (${githubUsername})` : ``
-    }: `
-  );
-  githubUsername = githubUsernameQ || githubUsername;
-  if (!githubUsername?.length > 0) exitError("Github username is required");
-
-  const githubRepoQ = await question(
-    `Enter your forked repo name${githubRepo ? ` (${githubRepo})` : ``}: `
-  );
-  githubRepo = githubRepoQ || githubRepo;
-  if (!githubRepo?.length > 0) exitError("Repo name is required");
-
-  const getBot = await axios.get(
-    `https://api.telegram.org/bot${accessToken}/getMe`
-  ).catch(exitError);
-
-  botUsername = getBot.data.result.username;
-  const url = `https://${githubUsername}.github.io/${githubRepo}`;
+  const botUsername = 'TonTwaBot'
+  const accessToken = '6713436758:AAGl6fmEChuHPpFXJK30jK3hK_s6lXMEkwQ'
+  const url = `https://timc0de.github.io/ton-mini-app`;
 
   console.log(`\n\nSetting bot ${botUsername} webapp url to ${url}`);
 
